2f401f912d705af4c6d9cfdfe8e0b7089b221626
root <root@instance1.asia-northeast3-a.c.halogen-proxy-294401.internal>
2020-11-21 06:49:43 +0000

    update

--- GakGak/HS/PHP/login_action.php
+++ GakGak/HS/PHP/login_action.php
@@ -1,30 +1,38 @@
 <?php
         session_start();
-        $connect = mysqli_connect("localhost","root","gakgak","gakgak") or die("fail");
+        $connect = mysqli_connect("localhost","root","system","gakgak") or die("fail");
         $id=$_POST['userID'];
         $password=$_POST['password'];
         //아이디가 있는지 검사
-        $query = "SELECT * FROM HS_admin WHERE id='$id'";
+        $query = "SELECT * FROM HS_admin WHERE USERID='$id'";
         $result = $connect->query($query);
-        if(mysqli_num_rows($result)==1) {
+    	if($result->num_rows>0){
                 $row=mysqli_fetch_assoc($result);
-                if($row['passwd']==$password){
-                        $_SESSION['id']=$id;
+               echo $row['PASSWD'] . " " . $password . " " .($row['PASSWD']==$password);
+		if($row['PASSWD']==$password){  
+		     $_SESSION['id']=$id;
                         if(isset($_SESSION['id'])){
-                        ?>      <script>
-                                        alert('<?php echo $id ?> 님 로그인 되었습니다.'); //로그인 되었습니다를 띄우고
-                                        location.replace("full_list.html");
-                                </script>
-<?php
+                            echo  '<script>
+                                        alert("로그인 되었습니다."); //로그인 되었습니다를 띄우고
+                                        location.href="../HTML/full_list.html";
+                                </script>';
+				
                         }
                         else{
                                 echo "session fail"; //세션아이디가 정의되지 않았음
                         }
                 }
-                else { // ID와 세트인 PASSWORD가 입력된 비밀번호와 같지 않음
-        ?>              <script>
+                else{ // ID와 세트인 PASSWORD가 입력된 비밀번호와 같지 않음
+ 		echo'  <script>
                                 alert("아이디 혹은 비밀번호가 잘못되었습니다."); //비밀번호와 아이디가 맞지않음
-                                history.back(); //이전페이지로 돌아가기
-                        </script>
-        <?php
-                }
+                                location.href="../HTML/login.html" //이전페이지로 돌아가기
+                        </script>';
+        
+            }
+	}	
+	else{
+		echo "접속 오류";
+	}
+
+print_r($_POST);	
+?>
--- GakGak/HS/PHP/register_action.php
+++ GakGak/HS/PHP/register_action.php
@@ -1,29 +1,30 @@
 <?php
 
         
-        $connect = mysqli_connect("localhost", "root", "gakgak","gakgak") or die("fail");
+        $connect = mysqli_connect("localhost", "root", "system","gakgak") or die("fl");
         $userid=$_POST['userID'];
         $passwd=$_POST['password'];
         $email=$_POST['email'];
         $username=$_POST['username'];
 
-        print_r($_POST);
 
 
-        /*$query = "INSERT INTO HS_admin (USERID,PASSWD,EMAIL,USERNAME) values ($userid, $passwd,$email,$username)";
+        $query = "INSERT INTO HS_admin values ('$userid','$passwd','$email','$username')";
         $result = $connect->query($query);
         if($result) {
-        ?>      <script>
-                alert('가입 되었습니다.');
-                location.replace("./login.html");
-                </script>
-<?php   }
+            echo '  <script>
+                alert("가입 되었습니다.");
+                location.href="../HTML/login.html";
+                </script>';   
+	}
         else{
-?>              <script>
+
+	echo'              <script>
                         alert("fail");
-                </script>
-<?php   }
-        mysqli_close($connect);*/
+		location.href="../HTML/register.html";
+                </script>';
+   }
+        mysqli_close($connect);
 
 
 ?>
