6ac121aae06eaaadd4b208531a20fd4880e1b915
Minji <gracemj805@gmail.com>
2020-11-21 14:28:00 +0900

    added full_list

---
+++ GakGak/HS/PHP/list_action.php
@@ -0,0 +1,27 @@
+<?php
+    session_start();
+    if ($_SESSION["userlogin"] == NULL){
+        header('Location: http://../HTML/login.html');
+    }
+
+    $con = mysqli_connect("34.123.128.137","duri","gakgak","gakgak");
+    if (mysqli_connect_errno())
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+
+    $result = mysql_query($con, "SELECT * FROM HS_list WHERE USERID=\'duri\'");
+    if (!$result){
+        die("Query to show fields from table failed");
+    }
+
+    echo "<tbody>";
+    while($row = mysqli_fetch_array($result)){
+        echo "<tr>";
+        echo "<td>" . $row['BUILDING'] . "</td>";
+        echo "<td>" . $row['LOCATION'] . "</td>";
+        echo "<td>" . $row['REMAINDER'] . "</td>";
+        echo "<td>" . $row['ORGAN'] . "</td>";
+        echo "<td></td>";
+        echo "</tr>";
+    }
+    echo "</tbody>";
+?>
