cb4072ffc61c22d810c398e1c1598be915383f4f
root <root@instance1.asia-northeast3-a.c.halogen-proxy-294401.internal>
2020-11-29 09:13:53 +0000

    hi

--- GakGak/HS/argon-dashboard-master/dash.php
+++ GakGak/HS/argon-dashboard-master/dash.php
@@ -40,7 +40,7 @@ $result=$connect->query($query);while ($row = mysqli_fetch_array($result)){
   
                          $orga=$row['ORGAN'];
                          $chga=$row['chg'];
-
+			$date=substr($date,10,6);
                          $per2=(int)$remaina/(int)$inita *100;
                          array_push($garr,$per2);
                          array_push($darr,$date);
@@ -181,7 +181,7 @@ $result=$connect->query($query);while ($row = mysqli_fetch_array($result)){
                 <div class="card-body">
                   <div class="row">
                     <div class="col">
-                      <h5 class="card-title text-uppercase text-muted mb-0">바꾼횟수</h5>
+                      <h5 class="card-title text-uppercase text-muted mb-0">Changed Time</h5>
                       <span class="h2 font-weight-bold mb-0"><?php echo $chg;?></span>
                     </div>
                     <div class="col-auto">
@@ -206,8 +206,8 @@ $result=$connect->query($query);while ($row = mysqli_fetch_array($result)){
             <div class="card-header bg-transparent">
               <div class="row align-items-center">
                 <div class="col">
-                  <h6 class="text-uppercase text-muted ls-1 mb-1">Performance</h6>
-                  <h5 class="h3 mb-0">Total orders</h5>
+                  <h6 class="text-uppercase text-muted ls-1 mb-1">Hand-Sanitizer</h6>
+                  <h5 class="h3 mb-0">Current Status</h5>
                 </div>
               </div>
             </div>
@@ -286,14 +286,14 @@ else{
             <div class="row align-items-center">
               <div class="col">
                 <h6 class="text-light text-uppercase ls-1 mb-1">Overview</h6>
-                <h5 class="h3 text-white mb-0">Sales value</h5>
+                <h5 class="h3 text-white mb-0">Changed Value per time</h5>
               </div>
               <div class="col">
                 <ul class="nav nav-pills justify-content-end">
-                  <li class="nav-item mr-2 mr-md-0" data-toggle="chart" data-target="#chart-sales-dark" data-update='{"data":{"datasets":[{"data":[0, 20, 10, 30, 15, 40, 20, 60, 60]}]}}' data-prefix="$" data-suffix="k">
+                  <li class="nav-item mr-2 mr-md-0" data-toggle="chart" data-target="#chart-sales-dark" data-update='{"data":{"datasets":[{"data":[0, 20, 10, 30, 15, 40, 20, 60, 60]}]}}' data-suffix="%">
 
                   </li>
-                  <li class="nav-item" data-toggle="chart" data-target="#chart-sales-dark" data-update='{"data":{"datasets":[{"data":[0, 20, 5, 25, 10, 30, 15, 40, 40]}]}}' data-prefix="$" data-suffix="k">
+                  <li class="nav-item" data-toggle="chart" data-target="#chart-sales-dark" data-update='{"data":{"datasets":[{"data":[0, 20, 5, 25, 10, 30, 15, 40, 40]}]}}' data-suffix="%">
                     
                   </li>
                 </ul>
@@ -1149,10 +1149,8 @@ var FormControl = (function() {
 // Google maps
 //
 var values="<?php echo $building;?>";
-let arr1=['"<?php echo $garr[0];?>"','"<?php echo $garr[2];?>"','"<?php echo $garr[5];?>"','"<?php echo $garr[8];?>"','"<?php echo $garr[11];?>"','"<?php echo $garr[14];?>"','"<?php echo $garr[17];?>"','"<?php echo $garr[20];?>"'];
-let arr2=['"<?php echo $darr[0];?>"','"<?php echo $darr[2];?>"','"<?php echo $darr[5];?>"','"<?php echo $darr[8];?>"','"<?php echo $darr[11];?>"','"<?php echo $darr[14];?>"','"<?php echo $darr[17];?>"','"<?php echo $darr[20];?>"'];
-
-
+let arr1=["<?php echo $garr[0];?>","<?php echo $garr[2];?>","<?php echo $garr[5];?>","<?php echo $garr[8];?>","<?php echo $garr[11];?>","<?php echo $garr[14];?>","<?php echo $garr[17];?>","<?php echo $garr[20];?>"];
+let arr2=["<?php echo $darr[0];?>","<?php echo $darr[2];?>","<?php echo $darr[5];?>","<?php echo $darr[8];?>","<?php echo $darr[11];?>","<?php echo $darr[14];?>","<?php echo $darr[17];?>","<?php echo $darr[20];?>"];
 var $map = $('#map-default'),
     map,
     lat,
@@ -1271,8 +1269,8 @@ var SalesChart = (function() {
             },
             ticks: {
               callback: function(value) {
-                if (!(value % 10)) {
-                  return '$' + value + 'k';
+                if (!(value % 20)) {
+                  return value + '%';
                 }
               }
             }
@@ -1296,11 +1294,10 @@ var SalesChart = (function() {
         }
       },
       data: {
-        
-        labels: [arr2[0], arr2[1], arr2[2], arr2[3], arr2[4], arr2[5], arr2[6], arr2[7]],
+        labels: [arr2[6], arr2[5], arr2[4], arr2[3], arr2[2], arr2[1], arr2[0]],
         datasets: [{
           label: 'Performance',
-          data: [arr1[0], arr1[1], arr1[2], arr1[3], arr1[4], arr1[5], arr1[6], arr1[7]]
+          data:[arr1[6], arr1[5], arr1[4], arr1[3], arr1[2], arr1[1], arr1[0]]
         }]
       }
     });
